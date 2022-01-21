c43c81de13f28655615188b92e939c0e33a54606
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2022-01-21 18:53:46 +0530

    chips card

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const removeChip = document.querySelector(".remove-chip");
-// const chips = document.querySelector(".chips");
-
-const removeElement = () => {
-  console.log("work");
-  // chips.display.style = "none";
-};
-
-removeChip.addEventListener("click", removeElement);
