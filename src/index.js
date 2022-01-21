7ed9e60cf5ab7d83de46900b4a6a42e4e2aa5a6a
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2022-01-21 18:52:50 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const removeChip = document.querySelector(".remove-chip");
+// const chips = document.querySelector(".chips");
+
+const removeElement = () => {
+  console.log("work");
+  // chips.display.style = "none";
+};
+
+removeChip.addEventListener("click", removeElement);
